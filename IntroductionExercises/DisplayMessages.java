b5f766bafda5d595472d16086b38afeb32a28ac1
ccbrantley <ccbrantley@gtcc.edu>
2018-01-04 14:24:25 -0500

    Create DisplayMessages.java

---
+++ IntroductionExercises/DisplayMessages.java
@@ -0,0 +1,7 @@
+public class forever {
+    public static void main (String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer");
+        System.out.println("Science, Programming is fun!");
+    }
+}
