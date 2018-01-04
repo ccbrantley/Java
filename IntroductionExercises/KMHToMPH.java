f517767b3fabbb862958c238e7d72cb219b2b08e
ccbrantley <ccbrantley@gtcc.edu>
2018-01-04 15:38:25 -0500

    Create KMHToMPH.java

---
+++ IntroductionExercises/KMHToMPH.java
@@ -0,0 +1,7 @@
+public class forever {
+    public static void main(String[] args){
+        double kmh = (14.0/45.5) * 60;
+        double mph = ((14.0/1.6)/45.5) * 60;
+        System.out.println("KMH: " + kmh + "\nMPH: " + mph);
+    }
+}
