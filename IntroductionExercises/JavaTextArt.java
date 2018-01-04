e2f32688aea745ef7b01d62205217d02d0236caf
ccbrantley <ccbrantley@gtcc.edu>
2018-01-04 14:43:14 -0500

    Create JavaTextArt.java

---
+++ IntroductionExercises/JavaTextArt.java
@@ -0,0 +1,8 @@
+public class forever {
+    public static void main (String[] args){
+        System.out.println("   J     A    V     V   A");
+        System.out.println("   J    AAA    V   V   AAA");
+        System.out.println("J  J   AAAAA    V V   AAAAA");
+        System.out.println(" JJ   A     A    V   A     A");
+    }
+}
