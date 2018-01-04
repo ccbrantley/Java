366c63b1dcc23f4d2dca99ec828664d47af6caef
ccbrantley <ccbrantley@gtcc.edu>
2018-01-04 14:56:31 -0500

    Create PowerOfExercise.java

---
+++ IntroductionExercises/PowerOfExercise.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+public class forever {
+    public static void main (String[] args){
+        System.out.println("a  a^2  a^3");
+        System.out.println(1 + "  " + 1 * 1 + "  " + 1 * 1 * 1);
+        System.out.println(2 + "  " + 2 * 2 + "  " + 2 * 2 * 2);
+        System.out.println(3 + "  " + 3 * 3 + "  " + 3 * 3 * 3);
+        System.out.println(4 + "  " + 4 * 4 + "  " + 4 * 4 * 4);
+        System.out.println(1 + "  " + Math.pow(1, 2) + "  " + Math.pow(1, 3));
+        System.out.println(2 + "  " + Math.pow(2, 2) + "  " + Math.pow(2, 3));
+        System.out.println(3 + "  " + Math.pow(3, 2) + "  " + Math.pow(3, 3));
+        System.out.println(4 + "  " + Math.pow(4, 2) + "  " + Math.pow(4, 3));
+    }
+}
