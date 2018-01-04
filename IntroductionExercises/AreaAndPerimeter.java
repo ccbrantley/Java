dabd2dd3f78a7031b9ab1b81e1fbe289b9087332
ccbrantley <ccbrantley@gtcc.edu>
2018-01-04 15:22:18 -0500

    Create AreaAndPerimeter.java

---
+++ IntroductionExercises/AreaAndPerimeter.java
@@ -0,0 +1,8 @@
+public class forever {
+    public static void main(String[] args){
+        double radius = 5.5;
+        double perimeter = 2 * radius * 3.14;
+        double area = radius * radius * 3.14;
+        System.out.println("Perimeter: " + perimeter +"\n" + "Area: " + area);
+    }
+}
