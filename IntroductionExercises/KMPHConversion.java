fada2a6409eb62a3c3360d3c34d80405458889b2
ccbrantley <ccbrantley@gtcc.edu>
2018-01-04 21:27:19 -0500

    Create KMPHConversion.java

---
+++ IntroductionExercises/KMPHConversion.java
@@ -0,0 +1,7 @@
+public class forever {
+    public static void main(String[] args){
+        double kmConversion = 25.0 / .62;
+        double kmphCalculation = (kmConversion/ 100.58) * 60;
+        System.out.println("KMPH: " + kmphCalculation);
+    }
+}
