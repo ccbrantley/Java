939eac56c1ef226732f4be80dc2f600f2c7cfd13
ccbrantley <ccbrantley@gtcc.edu>
2018-02-08 03:53:59 -0500

    Create Chemical.java

---
+++ ProductProject/Chemical.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public interface Chemical {
+    // Interface Methods
+    public String getChemicalName();
+
+    public void setChemicalName(String chemicalName);
+
+    public String getHazards();
+
+    public void setHazards(String hazards);
+
+    public String getPrecautions();
+
+    public void setPrecautions(String precautions);
+
+    public ArrayList<String> getUses();
+
+    public void setUses(ArrayList<String> uses);
+
+    public String getFirstAid();
+
+    public void setFirstAid(String firstAid);
+}
