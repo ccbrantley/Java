da91b5d26e405d1706633a627b4ddf75841470a9
ccbrantley <ccbrantley@gtcc.edu>
2018-02-08 17:04:16 -0500

    Update ThreeCards.java

--- JavaFxBasics/ThreeCards/ThreeCards.java
+++ JavaFxBasics/ThreeCards/ThreeCards.java
@@ -1,1 +1,30 @@
-
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import java.util.Random;
+public class ThreeCards extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(10,10,10,10));
+        Random rand = new Random();
+        int numArray[] = new int[3];
+        for (int i = 0; i < 3; i++) {
+           numArray[i]= rand.nextInt(52) + 1;
+        }
+        for (int i = 0; i < 3; i++){
+            pane.add(new ImageView(String.format("/card/%s.png",numArray[i])), i,0);
+        }
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Three Random Cards");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String [] args){
+        Application.launch(args);
+    }
+}
